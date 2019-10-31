eadc0bb687bfeee23522f36f65a744fc097e3e77
Steven Shave <stevenshave@gmail.com>
2019-10-31 15:58:44 +0000

    Reworked homo- vs hetero-dimer breaking plot

--- example-DisplayHomoVsHetroDimerComparisonHeatmaps.py
+++ example-DisplayHomoVsHetroDimerComparisonHeatmaps.py
@@ -29,34 +29,38 @@ colour_map = sns.color_palette("RdBu_r", 256)
 
 for i in range(len(heatmaps_hetero)):
     # ax[i,0].imshow(heatmaps_homo[i])
-    sns.heatmap(heatmaps_homo[i], ax=ax[0], vmin=0, vmax=1,
+    sns.heatmap(heatmaps_homo[i][:,::-1], ax=ax[0], vmin=0, vmax=1,
                 cmap=colour_map, cbar=True, cbar_kws=dict(pad=0.01))
-    sns.heatmap(heatmaps_hetero[i], ax=ax[1], vmin=0, vmax=1,
+    sns.heatmap(heatmaps_hetero[i][:,::-1], ax=ax[1], vmin=0, vmax=1,
                 cmap=colour_map, cbar=True, cbar_kws=dict(pad=0.01))
-    sns.heatmap(heatmaps_homo[i]-heatmaps_hetero[i], ax=ax[2],
-                cmap=colour_map, center=0, cbar=True, cbar_kws=dict(pad=0.01))
+    sns.heatmap(heatmaps_homo[i][:,::-1]-heatmaps_hetero[i][:,::-1], ax=ax[2],
+                cmap=sns.color_palette("PRGn", 1024), center=0, cbar=True, cbar_kws=dict(pad=0.01))
+ax[2].get_yaxis().set_visible(False)
 
 
-x_labels = ["-9", "-6", "-3"]
+x_labels = ["9 (nM)", "6 ($\mathrm{\mu}$M)", "3 (mM)"]
+x_labels.reverse()
 y_labels = x_labels.copy()
 
+y_labels.reverse()
+y_labels[2]=" 3 (mM)"
 for axx in ax:
     plt.xticks(
-        np.arange(0, heatmaps_hetero[0].shape[1]+1, heatmaps_hetero[0].shape[1]/2))
+        np.arange(0, heatmaps_hetero[0].shape[1]+1, heatmaps_hetero[0].shape[1]/2), rotation=0)
     plt.yticks(
         np.arange(0, heatmaps_hetero[0].shape[1]+1, heatmaps_hetero[0].shape[0]/2))
     axx.set_xticks = np.arange(len(x_labels))
     axx.set_yticks = np.arange(len(y_labels))
-    axx.set_xticklabels(x_labels)
-    axx.set_yticklabels(y_labels)
+    axx.set_xticklabels(x_labels, rotation=0)
+    axx.set_yticklabels(y_labels, rotation=0)
     axx.patch.set_linewidth('1')
     axx.patch.set_edgecolor('black')
 
-fig.text(0.45, 0.02, r'log$_{10} $K$_DI$')
-fig.text(0.008, 0.545, r'log$_{10}$ K$_DDimer}$', rotation=90)
-fig.text(0.131, 0.805, "Homodimer")
-fig.text(0.444, 0.805, "Heterodimer")
-fig.text(0.72, 0.805, "Homodimer-Heterodimer")
+fig.text(0.45, 0.02, r'p$\mathrm{K_DI}$')
+fig.text(0.008, 0.545, r'p$\mathrm{K_DDimer}$', rotation=90)
+fig.text(0.165, 0.805, "Homodimer")
+fig.text(0.465, 0.805, "Heterodimer")
+fig.text(0.68, 0.805, "Difference (Homodimer-Heterodimer)")
 
 plt.suptitle(r"Dimer breaking with inhibitor, 2 $\mathrm{\mu}$M homodimer monomer and" +
              "\n"+r"1+1 $\mathrm{\mu}$M heterodimer monomers, 5 $\mathrm{\mu}$M inhibitor", fontsize=16)
