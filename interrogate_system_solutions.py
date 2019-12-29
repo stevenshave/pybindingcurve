cfaf79725a9cebcac30e22076590efac431f96a1
Steven Shave <stevenshave@gmail.com>
2019-12-29 23:17:30 +0000

    Working interrogate_system_solutions

--- interrogate_system_solutions.py
+++ interrogate_system_solutions.py
@@ -1,21 +1,22 @@
 import pybindingcurve as pbc
 import random
 
+output_file = open("system_results.csv","w", buffering=1)
+output_file.write("p,l,kdpp,kdpl,solution")
 analytical_system = pbc.BindingCurve(pbc.systems.System_analytical_homodimerbreaking_pp)
 kinetic_system = pbc.BindingCurve(pbc.systems.System_kinetic_homodimerbreaking_pp)
 
-query_system={
-    'p':random.uniform(0.0,1000.0)*10**random.choice([0,-3,-6,-9,-12]),
-    'l':random.uniform(0.0,1000.0)*10**random.choice([0,-3,-6,-9,-12]),
-    'kdpl':random.uniform(0.0,1000.0)*10**random.choice([0,-3,-6,-9,-12]),
-    'kdpp':random.uniform(0.0,1000.0)*10**random.choice([0,-3,-6,-9,-12])
-    }
-print(query_system)
-kinetic_result=kinetic_system.query(query_system)
-analytical_result=analytical_system.query(query_system)
+while(True):
+    query_system={
+        'p':random.uniform(0.0,1000.0)*10**random.choice([0,-3,-6,-9,-12]),
+        'l':random.uniform(0.0,1000.0)*10**random.choice([0,-3,-6,-9,-12]),
+        'kdpl':random.uniform(0.0,1000.0)*10**random.choice([0,-3,-6,-9,-12]),
+        'kdpp':random.uniform(0.0,1000.0)*10**random.choice([0,-3,-6,-9,-12])
+        }
+    kinetic_result=kinetic_system.query(query_system)
+    analytical_result=analytical_system.query(query_system)
 
-print(f"Kinetic {kinetic_result}")
-print(f"Analytical {analytical_result}")
+    closest = min(range(len(analytical_result)), key=lambda i: abs(analytical_result[i]-kinetic_result))
+    output_file.write(f"{query_system['p']},{query_system['l']},{query_system['kdpp']},{query_system['kdpl']},{closest}\n")
 
-closest = min(range(len(analytical_result)), key=lambda i: abs(analytical_result[i]-kinetic_result))
-print(f"Index = {closest}")
+output_file.close()
