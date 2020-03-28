a0a4c808a1dfa4c56798d6c04ecb57985351ab6c
Steven Shave <stevenshave@gmail.com>
2020-03-28 15:39:51 +0000

    Updated setup scripts

--- setup.py
+++ setup.py
@@ -1,24 +1,23 @@
-import io
-import os
-import re
+import setuptools
 
-from setuptools import find_packages
-from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
-
-def read(filename):
-    filename = os.path.join(os.path.dirname(__file__), filename)
-    text_type = type(u"")
-    with io.open(filename, mode="r", encoding="utf-8") as fd:
-        return re.sub(text_type(r":[a-z]+:`~?(.*?)`"), text_type(r"``\1``"), fd.read())
-
-
-from distutils.core import setup
-
-setup(
-    name="pyBindingCurve",
-    version="0.2.0",
-    packages=find_packages(exclude=("tests",)),
-    license="The MIT License (MIT)",
-    long_description=open("README.md").read(),
+setuptools.setup(
+    name="PyBindingCurve",
+    version="0.2.1",
+    author="Steven Shave",
+    author_email="steve.nshave@gmail.com",
+    description="Protein ligand binding simulation in Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/stevenshave/pybindingcurve",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
 )
+
