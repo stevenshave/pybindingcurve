04f7d45eebbf2e7427f89ea8c9b2792af429e6bd
Steven Shave <stevenshave@gmail.com>
2020-12-11 22:09:45 +0000

    New version on pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pybindingcurve",
-    version="1.0.4",
+    version="1.0.5",
     author="Steven Shave",
     author_email="steve.nshave@gmail.com",
     description="Protein ligand binding simulation in Python",
