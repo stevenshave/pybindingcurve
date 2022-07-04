598eb00a5cb48c4adcd14b5aae899473053b07a2
Steven Shave <stevenshave@gmail.com>
2022-07-04 07:08:12 +0100

    Version number matching pypi package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pybindingcurve",
-    version="1.1.0",
+    version="1.1.1",
     author="Steven Shave",
     author_email="steve.nshave@gmail.com",
     description="Protein ligand binding simulation in Python",
