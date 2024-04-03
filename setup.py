a06c357706fd4d4b630b56ab8d021ae08b2915f5
Steven Shave <stevenshave@gmail.com>
2024-04-03 16:30:09 +0100

    Packaging with pyproject.toml

--- setup.py
+++ setup.py
@@ -1,29 +1,3 @@
-import setuptools
+from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="pybindingcurve",
-    version="1.1.4",
-    author="Steven Shave",
-    author_email="steve.nshave@gmail.com",
-    description="Protein ligand binding simulation in Python",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/stevenshave/pybindingcurve",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        "numpy>=1.22",
-        "matplotlib>=3.2.1",
-        "lmfit>=1.0.0",
-        "mpmath>=1.1.0",
-        "autograd>=1.3",
-    ],
-    python_requires=">=3.7",
-)
+setup()
